b564b72a7a26e38fa2ad4b7293efee2bdccf59ef
asperkins42 <158613707+asperkins42@users.noreply.github.com>
2024-09-26 11:10:11 -0500

    Add files via upload

--- hw1/kernel.c
+++ hw1/kernel.c
@@ -1,1 +1,26 @@
+#include "libos.h"
+
+int clear_scr(int start_row, int start_col, int width, int height){};	
+int box(unsigned int srow, unsigned int scol, unsigned int erow, unsigned int ecol){};
+
+
+
+int main () {
+	
+	int start_row = 21;
+	int start_col = 49;
+	int end_row = 27;
+	int end_col = 79;
+	
+	unsigned int srow = 21;
+	unsigned int scol = 49;
+	unsigned int erow = 27;
+	unsigned int ecol = 79;
+	
+	
+	// call clear_scr with the start row, start column, end row, and end column
+	// call box with the start row, start column, end row, and end column
+	// call print_to with the row and column and string to print
+
+}
 
