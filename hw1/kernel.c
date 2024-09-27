ec182be5569c5be0270ba8e26276f453f733c0fd
asperkins42 <158613707+asperkins42@users.noreply.github.com>
2024-09-26 23:23:20 -0500

    Add files via upload

--- hw1/kernel.c
+++ hw1/kernel.c
@@ -1,42 +1,8 @@
 #include "libos.h"
-#include <stdlib.h> //to use exit()
+#include <stdio.h>
 
+int box(unsigned int srow, unsigned int scol, unsigned int erow, unsigned int ecol); 
 int clear_scr(int start_row, int start_col, int width, int height);	
-int box(unsigned int srow, unsigned int scol, unsigned int erow, unsigned int ecol);
-
-
-
-int main () {
-	
-	// u_int declarations for box function
-	unsigned int u_srow = 21;
-	unsigned int u_scol = 49;
-	unsigned int u_erow = 27;
-	unsigned int u_ecol = 79;
-
-	// int declarations for clear_scr
-	int srow = 21;
-	int scol = 49;
-	int erow = 27; 
-	int ecol = 79;
-
-	// variable declarations for print_to
-	unsigned int char_row = 24;
-	unsigned int char_col = 59;
-	char text[] = "Hello World";
-
-	// clear_scr function -- C
-	clear_scr(srow, scol, erow, ecol);
-	
-	// call box function -- ASSEMBLY
-	box(u_srow, u_scol, u_erow, u_ecol);
-
-	// write Hello World to screen
-	print_to(char_row, char_col, text);
-	
-	return 0;
-
-}
 
 // clear_scr function
 int clear_scr(int srow,  int scol,  int erow,  int ecol) {
@@ -52,8 +18,8 @@ int clear_scr(int srow,  int scol,  int erow,  int ecol) {
 	}
 
 	// if parameters are valid, clear the screen then return to the function
-	for (int i = srow; i < erow; i++) {
-		for (int k = scol; k < ecol; k++) {
+	for (int i = srow; i <= erow; i++) {
+		for (int k = scol; k <= ecol; k++) {
 
 			putc_to(i, k, empty);
 
@@ -63,3 +29,39 @@ int clear_scr(int srow,  int scol,  int erow,  int ecol) {
 	return 0;
 }
 
+
+int main() {
+
+	// variable declarations
+	unsigned int srow = 21; 
+	unsigned int scol = 49; 
+	unsigned int erow = 27; 
+	unsigned int ecol = 79; 
+
+	// variables for clear screen
+	int s_row = 21;
+	int s_col = 49;
+	int e_row = 27;
+	int e_col = 79;
+
+	// clear the screen
+	clear_scr(s_row, s_col, e_row, e_col);
+
+	// set finished to 1 or 0 depending on if errors occured drawing the box
+	int finished = box(srow, scol, erow, ecol); 
+
+		if (finished == 0) {
+
+			// if we finish and are good...
+			print_to(24, 59, "Hello World"); 
+
+		} else {
+
+			// if we finish and are not...
+			print_to(24, 59, "Broken D:"); 
+		}
+
+
+	return 0;  
+
+}
